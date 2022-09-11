76a090ce98cf6d6caeb05b7965db5caaf7a974a0
lhikarul <711hikaru@gmail.com>
2022-09-11 15:34:56 +0800

    feat: each

--- src/modules/each/index.js
+++ src/modules/each/index.js
@@ -2,18 +2,11 @@ import isArrayLike from "../isArrayLike/index.js";
 import keys from "../keys/index.js";
 
 export default function each(obj, iteratee) {
-  let i, length;
-  if (isArrayLike(obj)) {
-    length = obj.length;
-    for (i = 0; i < length; i++) {
-      iteratee(obj[i], i, obj);
-    }
-  } else {
-    let _keys = keys(obj);
-    length = _keys.length;
-    for (i = 0; i < length; i++) {
-      iteratee(obj[_keys[i]], _keys[i], obj);
-    }
+  let _keys = !isArrayLike(obj) && keys(obj);
+  let length = (_keys || obj).length;
+
+  for (let i = 0; i < length; i++) {
+    let currentKey = _keys ? _keys[i] : i;
+    iteratee(obj[currentKey], currentKey);
   }
-  return obj;
 }
