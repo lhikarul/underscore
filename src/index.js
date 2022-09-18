5c121fbedc85d532f94c74abc200c7a26faf33f1
lhikarul <711hikaru@gmail.com>
2022-09-18 15:34:23 +0800

    feat: intersection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import isArrayLike from "./modules/isArrayLike";
 import keys from "./modules/keys";
-import intersection from "./modules/intersection";
+// import intersection from "./modules/intersection";
+
+function contains(array, value) {
+  let length = array.length,
+    index = 0;
+  for (index = 0; index < length; index++) {
+    if (array[index] === value) return true;
+  }
+  return false;
+}
+
+function intersection(array) {
+  let length = array.length,
+    argsLength = arguments.length,
+    result = [],
+    index = 0;
+
+  for (index = 0; index < length; index++) {
+    let item = array[index];
+    if (contains(result, item)) continue;
+
+    let j = 1;
+
+    for (; j < argsLength; j++) {
+      if (!contains(arguments[j], item)) break;
+    }
+
+    if (j === argsLength) result.push(item);
+  }
+
+  return result;
+}
 
 console.log(
   intersection([1, 1, 1, 1, 1, 2, 1111], [1, 1, 1, 1, 4, 1111], [1111, 2, 4, 1])
--- src/modules/intersection/index.js
+++ src/modules/intersection/index.js
@@ -1,5 +1,8 @@
 import contains from "../contains";
 
+// intersection([1, 1, 1, 1, 1, 2, 1111], [1, 1, 1, 1, 4, 1111], [1111, 2, 4, 1])
+// -> [1,1111]
+
 export default function intersection(array) {
   const length = array.length;
   const argsLength = arguments.length;
@@ -8,11 +11,14 @@ export default function intersection(array) {
 
   for (let index = 0; index < length; index++) {
     let item = array[index];
+    // 若 result 已經包含 item，跳過此迴圈
     if (contains(result, item)) continue;
     let j;
     for (j = 1; j < argsLength; j++) {
+      // arguments[j] 的陣列沒有包含 item，直接中斷迴圈，因為沒有滿足交集
       if (!contains(arguments[j], item)) break;
     }
+    // 確保 item 是所有陣列的交集
     if (j === argsLength) result.push(item);
   }
 
